fd73be73f05b53952156573b66fd1dbd38a99af9
mazaherireza <rezamazaheri.email@gmail.com>
2025-12-14 08:30:06 +0330

    Reset CSS

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fa" dir="rtl" className={vazirmatn.className}>
-      <body>{children}</body>
+      <body>
+        <header></header>
+        <main>{children}</main>
+        <footer></footer>
+      </body>
     </html>
   );
 }
