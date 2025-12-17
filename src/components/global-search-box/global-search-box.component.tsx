b7c02298b61402c21b25d3f68ff8ce8516629e32
mazaherireza <rezamazaheri.email@gmail.com>
2025-12-17 19:46:43 +0330

    Correct Mistyping

---
+++ src/components/global-search-box/global-search-box.component.tsx
@@ -0,0 +1,24 @@
+import type { ReactNode } from "react";
+
+import { LocationIcon } from "@/assets/icons/location.icon";
+import { SearchIcon } from "@/assets/icons/search.icon";
+
+import styles from "./global-search-box.module.css";
+
+export default function GlobalSerachBoxComponent(): ReactNode {
+  return (
+    <div className={styles["global-search-box"]}>
+      <div className={styles.prefix}>
+        <SearchIcon />
+      </div>
+      <input type="text" placeholder="نام بیماری، تخصص، پزشک ..." />
+      <div className={styles.divider}></div>
+      <div className={styles.suffix}>
+        <button>
+          <LocationIcon />
+          همه شهرها
+        </button>
+      </div>
+    </div>
+  );
+}
