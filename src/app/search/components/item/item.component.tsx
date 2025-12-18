df4cc85f6753c19a05b4c5d6dc35ee5bd1fbd85b
mazaherireza <rezamazaheri.email@gmail.com>
2025-12-18 20:38:05 +0330

    List Component

--- src/app/search/components/item/item.component.tsx
+++ src/app/search/components/item/item.component.tsx
@@ -1,45 +1,45 @@
 "use client";
 
-import { type ReactNode, useContext, useMemo } from "react";
+import { type ReactNode } from "react";
 
-import clsx from "clsx";
+import { ItemType } from "@/types/item.type";
 
-import { FiltersContext } from "@/app/search/providers/filters/filters-provider";
+// import clsx from "clsx";
+
+// import { FiltersContext } from "@/app/search/providers/filters/filters-provider";
 
 import styles from "./item.module.css";
 
 type Props = {
-  item: number;
+  item: ItemType;
 };
 
 export default function ItemComponent({ item }: Props): ReactNode {
-  const { filters } = useContext(FiltersContext);
+  // const { filters } = useContext(FiltersContext);
 
-  const isActive = useMemo(() => {
-    if (filters.even && item % 2 === 0) {
-      return true;
-    }
+  // const isActive = useMemo(() => {
+  //   if (filters.even && item.value % 2 === 0) {
+  //     return true;
+  //   }
 
-    if (filters.odd && item % 2 === 1) {
-      return true;
-    }
+  //   if (filters.odd && item.value % 2 === 1) {
+  //     return true;
+  //   }
 
-    if (filters.three && item % 3 === 0) {
-      return true;
-    }
+  //   if (filters.three && item.value % 3 === 0) {
+  //     return true;
+  //   }
 
-    if (filters.five && item % 5 === 0) {
-      return true;
-    }
+  //   if (filters.five && item.value % 5 === 0) {
+  //     return true;
+  //   }
 
-    if (filters.seven && item % 7 === 0) {
-      return true;
-    }
+  //   if (filters.seven && item.value % 7 === 0) {
+  //     return true;
+  //   }
 
-    return false;
-  }, [filters, item]);
+  //   return false;
+  // }, [filters, item]);
 
-  return (
-    <li className={clsx(styles.item, isActive && styles.active)}>{item}</li>
-  );
+  return <li className={styles.item}>{item.value}</li>;
 }
