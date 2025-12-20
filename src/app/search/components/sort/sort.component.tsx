c03617ab12a9dfacfff9f6c51e17a60e1089b06f
mazaherireza <rezamazaheri.email@gmail.com>
2025-12-20 19:19:05 +0330

    Implement Sort Component

---
+++ src/app/search/components/sort/sort.component.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { type ReactNode, useState } from "react";
+
+import SelectComponent from "@/components/select/select.component";
+
+import { SelectOptionType } from "@/types/select-option.type";
+
+const options: SelectOptionType[] = [
+  { value: "rating", label: "بهترین" },
+  { value: "popularity", label: "محبوب‌ترین" },
+  { value: "appointment", label: "نزدیک‌ترین نوبت" },
+  { value: "waiting", label: "کمترین زمان معطلی" },
+  { value: "view", label: "پربازدیدترین" },
+];
+
+export default function SortComponent(): ReactNode {
+  const [selectedOption, setSelectedOption] = useState<SelectOptionType>(
+    options[0]
+  );
+  return (
+    <SelectComponent
+      title="مرتب‌سازی"
+      options={options}
+      floating
+      selectedOption={selectedOption}
+      onSelectedOptionChange={setSelectedOption}
+    />
+  );
+}
