8cefa6f79ca1be62ab8d9494aec8626f246c95a2
mazaherireza <rezamazaheri.email@gmail.com>
2026-01-08 22:08:01 +0330

    Implement Normal Input Component

---
+++ src/components/normal-input/normal-input.component.tsx
@@ -0,0 +1,49 @@
+import { type ComponentProps, type ReactNode, forwardRef, type ForwardedRef } from "react";
+
+import clsx from "clsx";
+
+import { ButtonComponent } from "@/components/button/button.component";
+
+import styles from "./normal-input.module.css";
+
+type Props = ComponentProps<"input"> & {
+  label: string;
+  prefixIcon?: ReactNode;
+  suffixIcon?: ReactNode;
+  onSuffixClick?: ComponentProps<typeof ButtonComponent>["onClick"];
+};
+
+function NormalInputComponent(
+  {
+    label,
+    prefixIcon,
+    suffixIcon,
+    onSuffixClick,
+    className,
+    ...otherProps
+  }: Props,
+  ref: ForwardedRef<HTMLInputElement>
+): ReactNode {
+  return (
+    <label className={clsx(styles["normal-input"], className)}>
+      <div className={styles["label-text"]}>{label}</div>
+      <div className={styles.box}>
+        {prefixIcon && (
+          <div className={styles["prefix-icon"]}>{prefixIcon}</div>
+        )}
+        <input ref={ref} placeholder="" {...otherProps} />
+        {suffixIcon && (
+          <ButtonComponent
+            type="button"
+            shape="inherit"
+            onClick={onSuffixClick}
+          >
+            <div className={styles["suffix-icon"]}>{suffixIcon}</div>
+          </ButtonComponent>
+        )}
+      </div>
+    </label>
+  );
+}
+
+export default forwardRef(NormalInputComponent);
