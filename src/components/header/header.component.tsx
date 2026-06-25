88ff7e7bd521b1d51eae687a1f587e2da38153dd
mazaherireza <rezamazaheri.email@gmail.com>
2026-06-25 10:48:31 +0330

    Use LinkButtonComponent

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -5,6 +5,8 @@ import type { ReactNode } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
+import { ButtonLinkComponent } from "@/components/button/button.component";
+
 import clsx from "clsx";
 
 import styles from "./header.module.css";
@@ -31,7 +33,14 @@ export default function HeaderComponent(): ReactNode {
           </li>
         </ul>
       </nav>
-      <button className={styles.cta}>ورود | ثبت‌نام</button>
+      <ButtonLinkComponent
+        className={styles.cta}
+        variant="primary"
+        shape="outlined"
+        href="/auth/sign-in"
+      >
+        ورود | ثبت‌نام
+      </ButtonLinkComponent>
     </header>
   );
 }
