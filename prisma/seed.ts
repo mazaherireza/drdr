36e2b9b54215a45e186f439c051e34b02cfa1cb8
mazaherireza <rezamazaheri.email@gmail.com>
2026-06-25 09:18:57 +0330

    Add Seed

---
+++ prisma/seed.ts
@@ -0,0 +1,34 @@
+import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+
+const prisma = new PrismaClient({
+  adapter,
+});
+
+export async function main() {
+  await prisma.user.deleteMany();
+
+  const users = await prisma.user.createMany({
+    data: [
+      {
+        name: "Reza Mazaheri",
+        username: "rezamazaheri",
+        email: "rezamazaheri.email@gmail.com",
+        password: "1001",
+      },
+    ],
+  });
+}
+
+main()
+  .catch((e) => {
+    console.error("Error seeding data:", e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
