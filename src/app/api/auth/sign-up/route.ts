52807fb024956daaeab2c034340635b1e42e7c25
mazaherireza <rezamazaheri.email@gmail.com>
2026-06-26 12:41:03 +0330

    Use wrapWithTryCatch

--- src/app/api/auth/sign-up/route.ts
+++ src/app/api/auth/sign-up/route.ts
@@ -2,27 +2,18 @@ import { NextResponse, NextRequest } from "next/server";
 
 import { prisma } from "@/lib/prisma";
 
-export async function POST(request: NextRequest): Promise<NextResponse> {
-  try {
+import { wrapWithTrycatch } from "@/utils/fetch.util";
+
+import type { ApiResponseType } from "@/types/api-response.type";
+
+export async function POST(
+  request: NextRequest,
+): Promise<ApiResponseType<null>> {
+  return wrapWithTrycatch(async () => {
     const body = await request.json();
-    console.log(body);
 
     await prisma.user.create({ data: body });
 
-    return NextResponse.json(
-      { message: "User created successfully." },
-      { status: 201 },
-    );
-  } catch (error) {
-    if (error instanceof Error) {
-      return NextResponse.json({ error: error.message }, { status: 400 });
-    }
-
-    return NextResponse.json(
-      { error: "Something went wrong" },
-      {
-        status: 500,
-      },
-    );
-  }
+    return NextResponse.json({ data: null }, { status: 201 });
+  });
 }
