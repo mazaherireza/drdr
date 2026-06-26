0a06a385e452ff2e109edf54d9bf73957b066f39
mazaherireza <rezamazaheri.email@gmail.com>
2026-06-26 15:23:21 +0330

    Use parseBody

--- src/app/api/auth/sign-up/route.ts
+++ src/app/api/auth/sign-up/route.ts
@@ -3,16 +3,45 @@ import { NextResponse, NextRequest } from "next/server";
 import { prisma } from "@/lib/prisma";
 
 import { wrapWithTrycatch } from "@/utils/fetch.util";
+import { parseBody } from "@/utils/fetch.util";
 
 import type { ApiResponseType } from "@/types/api-response.type";
+
+import { SignUpDto } from "@/dto/auth.dto";
 
 export async function POST(
   request: NextRequest,
 ): Promise<ApiResponseType<null>> {
   return wrapWithTrycatch(async () => {
-    const body = await request.json();
+    const [error, data] = await parseBody<SignUpDto>(request);
 
-    await prisma.user.create({ data: body });
+    if (error !== null) {
+      return NextResponse.json({ error }, { status: 400 });
+    }
+
+    let user = await prisma.user.findUnique({
+      where: { username: data.username },
+    });
+
+    if (user) {
+      return NextResponse.json(
+        { error: "username has already taken." },
+        { status: 400 },
+      );
+    }
+
+    user = await prisma.user.findUnique({
+      where: { username: data.email },
+    });
+
+    if (user) {
+      return NextResponse.json(
+        { error: "email has already taken." },
+        { status: 400 },
+      );
+    }
+
+    await prisma.user.create({ data });
 
     return NextResponse.json({ data: null }, { status: 201 });
   });
