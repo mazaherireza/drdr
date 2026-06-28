d9c37c6412c1257ec2a3037046b2f92d6dc8295f
mazaherireza <rezamazaheri.email@gmail.com>
2026-06-28 15:40:50 +0330

    Update CardComponent

--- src/components/card/card.component.tsx
+++ src/components/card/card.component.tsx
@@ -1,9 +1,23 @@
 import type { PropsWithChildren, ReactNode } from "react";
 
+import clsx from "clsx";
+
 import styles from "./card.module.css";
 
-type Props = PropsWithChildren;
+type Props = PropsWithChildren<{
+  className?: string;
+  title?: string;
+}>;
 
-export default function CardComponet({ children }: Props): ReactNode {
-  return <div className={styles.card}>{children}</div>;
+export default function CardComponet({
+  children,
+  className,
+  title,
+}: Props): ReactNode {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.title}>{title}</div>
+      <div className={clsx(styles.card, className)}>{children}</div>
+    </div>
+  );
 }
