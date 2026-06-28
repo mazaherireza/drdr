7d0064a8a9b491d57de580586019b43fccf2f872
mazaherireza <rezamazaheri.email@gmail.com>
2026-06-28 10:52:34 +0330

    Rename File

--- src/utils/convert-en-num-to-fa.ts
+++
@@ -1,7 +0,0 @@
-export function convertEnNumToFa(text: string): string {
-  const splitNumber = text.toString().split("");
-  const persianNumber = ["۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹"];
-  return splitNumber
-    .map((num) => (/[0-9]/.test(num) ? persianNumber[Number(num)] : num))
-    .join("");
-}
