d6a4119ab1e14092602c3dac5d1b05ea6af668ea
mazaherireza <rezamazaheri.email@gmail.com>
2026-06-28 10:56:25 +0330

    Update Content

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { type ReactNode } from "react";
+
+import { useRouter } from "next/navigation";
+
+import { fetchWithToast } from "@/utils/fetch.util";
+
+import { ButtonComponent } from "@/components/button/button.component";
+
+import styles from "./page.module.css";
+
+export default function Page(): ReactNode {
+  const router = useRouter();
+
+  const handleSignOutButtonClick = async (): Promise<void> => {
+    const result = await fetchWithToast<null>("/api/sign-out", {
+      method: "DELETE",
+    });
+
+    if (result?.error) {
+      return;
+    }
+
+    router.push("/");
+  };
+
+  return (
+    <div className={styles.page}>
+      <h1>داشبورد</h1>
+      <ButtonComponent variant="danger" onClick={handleSignOutButtonClick}>
+        خروج
+      </ButtonComponent>
+    </div>
+  );
+}
