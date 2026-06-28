f8beaf4c0428779f8fbe46b322a2eb5aa91680e3
mazaherireza <rezamazaheri.email@gmail.com>
2026-06-28 10:49:24 +0330

    Add proxy

---
+++ src/proxy.ts
@@ -0,0 +1,29 @@
+import type { NextRequest } from "next/server";
+
+import { NextResponse } from "next/server";
+
+import { isSignedIn } from "@/utils/cookie.util";
+
+const onlySignedInRoutes = ["/dashboard"];
+const onlyNotSignedInRoutes = ["/auth/sign-in", "/auth/sign-up"];
+
+export default async function proxy(request: NextRequest) {
+  const pathname = request.nextUrl.pathname;
+
+  const isOnlySignedInRoutes = onlySignedInRoutes.includes(pathname);
+  const isOnlyNotSignedInRoutes = onlyNotSignedInRoutes.includes(pathname);
+
+  if (await isSignedIn(request)) {
+    if (isOnlyNotSignedInRoutes && !pathname.startsWith("/dashboard")) {
+      return NextResponse.redirect(new URL("/dashboard", request.url));
+    }
+  } else {
+    if (isOnlySignedInRoutes && !pathname.startsWith("/auth/sign-in")) {
+      return NextResponse.redirect(new URL("/auth/sign-in", request.url));
+    }
+  }
+}
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+};
